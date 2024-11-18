3eb99a36671e1d88fc4af9015df962a7c082c62f
aayyuusshhiii <105377829+aayyuusshhiii@users.noreply.github.com>
2024-11-18 21:35:09 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,3 +20,4 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('sticky');
     }
 });
+// You can add interactivity later if needed. For now, this file can be left empty or used for additional features.
